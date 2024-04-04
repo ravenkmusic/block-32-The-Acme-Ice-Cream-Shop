5ceb30a1283ad25b370dd0306531644bd9ca2dda
Raven Hendricks <ravenkbeats@gmail.com>
2024-04-04 15:57:12 -0400

    added temporary logic for init function, confirmed app is running on port

--- index.js
+++ index.js
@@ -5,8 +5,34 @@ const app = express();
 
 //deployment routes
 
+
 //crud routes
+
+//get
+//post
+//put 
+//delete
 
 //database connection
 
+const init = async() => {
+    //establish and confirm connection
+    await client.connect();
+    console.log("Database connected.");
+
+    //create tables
+    let SQL = ``;
+
+    //establish and confirm table creation
+    await client.query(SQL);
+    console.log("Tables created.");
+
+    let aSQL = ``;
+    await client.query(aSQL);
+    console.log("Info seeded.");
+    const port = process.env.port || 5000
+    app.listen(port, ()=> console.log(`Now listening on port ${port}`));
+}
+
 //invoking database connection
+init();
