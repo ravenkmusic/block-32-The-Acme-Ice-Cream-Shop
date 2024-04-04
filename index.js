5c9bb7fef06f4de4530950a0dc1d6f3117b46c95
Raven Hendricks <ravenkbeats@gmail.com>
2024-04-04 16:25:52 -0400

    created API

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = express();
 
 //deployment routes
 
+//parses incoming requests
+app.use(express.json());
+
+//logs incoming requests
+app.use(require('morgan')(dev));
 
 //crud routes
 
@@ -21,15 +26,35 @@ const init = async() => {
     console.log("Database connected.");
 
     //create tables
-    let SQL = ``;
+    let SQL = `
+        DROP TABLE IF EXISTS flavors;
+        CREATE TABLE flavors(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            is_favorite BOOLEAN DEFAULT FALSE,
+            created_at TIMESTAMP DEFAULT now(),
+            updated_at TIMESTAMP DEFAULT now()
+        )
+    `;
 
     //establish and confirm table creation
     await client.query(SQL);
     console.log("Tables created.");
-
-    let aSQL = ``;
-    await client.query(aSQL);
+    SQL = `
+        INSERT INTO flavors (name, is_favorite) VALUES('Chocolate', false);
+        INSERT INTO flavors (name, is_favorite) VALUES('Coffee', false);
+        INSERT INTO flavors (name, is_favorite) VALUES('Vanilla', true);
+        INSERT INTO flavors (name, is_favorite) VALUES('Oreo', true);
+        INSERT INTO flavors (name, is_favorite) VALUES('Rocky Road', false);
+        INSERT INTO flavors (name, is_favorite) VALUES('Orange Creamsicle', false);
+        INSERT INTO flavors (name, is_favorite) VALUES('Strawberry', false);
+        INSERT INTO flavors (name, is_favorite) VALUES('Neopolitan', false);
+        INSERT INTO FLAVORS (name, is_favorite) VALUES('Rum Raisin', false);
+    `;
+    await client.query(SQL);
     console.log("Info seeded.");
+
+    //establish and confirm listening on port 5000
     const port = process.env.port || 5000
     app.listen(port, ()=> console.log(`Now listening on port ${port}`));
 }
