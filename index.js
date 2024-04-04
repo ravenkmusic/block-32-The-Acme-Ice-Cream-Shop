c423239bd977b0bbf7bc4e6abb3bffdad7abda5a
Raven Hendricks <ravenkbeats@gmail.com>
2024-04-04 15:48:17 -0400

    added comments to help keep track of construction

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const pg = require('pg');
 const express = require('express');
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/the_acme_notes_db');
 const app = express();
+
+//deployment routes
+
+//crud routes
+
+//database connection
+
+//invoking database connection
